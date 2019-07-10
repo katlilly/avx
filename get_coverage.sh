a392eea4a3cc4f48c40babf8833cc46740d9a737
Kat <kat.lilly@gmail.com>
2019-07-10 19:45:05 +1200

    code coverage

---
+++ get_coverage.sh
@@ -0,0 +1,6 @@
+g++ -march=native -std=c++11 -fprofile-arcs -ftest-coverage -o testavxSimple10 simple10avx.cpp testavxsimple10.cpp
+
+./testavxSimple10
+gcov simple10avx.cpp testavxsimple10.cpp
+
+#-march=knl ?? or sandybridge 
