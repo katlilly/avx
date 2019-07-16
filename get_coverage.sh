d71380f0d48afc2d408284f2365d1659da918e9a
kat <kat.lilly@gmail.com>
2019-07-17 11:28:54 +1200

    one last try at avx512 build script

--- get_coverage.sh
+++ get_coverage.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-g++ -march=native -std=c++11 -fprofile-arcs -ftest-coverage -o testavxSimple10 simple10avx.cpp testavxsimple10.cpp
+#export CODECOV_TOKEN="d330997c-36e9-4a4a-bf7f-0a5fb83fd603"
 
-./testavxSimple10
-gcov simple10avx.cpp testavxsimple10.cpp
+g++ -march=native -std=c++11 -fprofile-arcs -ftest-coverage -o regtest regtest.cpp simple10avx.cpp
 
-#-march=knl ?? or sandybridge 
+./regtest
+gcov regtest.cpp simple10avx.cpp
+
+#bash <(curl -s https://codecov.io/bash)
+
+bash <(curl -s https://codecov.io/bash) -t d330997c-36e9-4a4a-bf7f-0a5fb83fd603
